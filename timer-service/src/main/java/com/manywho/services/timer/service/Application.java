793580adc2bf62c5b3fd0ab0351734f950f7a805
Jonjo McKay <jonjo.mckay@manywho.com>
2016-01-07 11:05:33 +0000

    Changed packaging to be JAR by default

--- timer-service/src/main/java/com/manywho/services/timer/service/Application.java
+++ timer-service/src/main/java/com/manywho/services/timer/service/Application.java
@@ -2,17 +2,17 @@ package com.manywho.services.timer.service;
 
 import com.manywho.sdk.services.BaseApplication;
 
-import javax.servlet.ServletContext;
 import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Context;
 
 @ApplicationPath("/")
 public class Application extends BaseApplication {
-    @Context
-    ServletContext servletContext;
 
     public Application() {
         registerSdk().packages("com.manywho.services.timer")
                 .register(new ApplicationBinder());
+    }
+
+    public static void main(String[] args) {
+        startServer(new Application(), "api/timer/1");
     }
 }
