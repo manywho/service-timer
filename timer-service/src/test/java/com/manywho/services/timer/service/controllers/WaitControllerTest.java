95b132eb5df805dd65a6ad77699a4ffb70c27d9d
Jonjo McKay <jonjo@jonjomckay.com>
2020-06-02 14:42:52 +0100

    Fixed tests

--- timer-service/src/test/java/com/manywho/services/timer/service/controllers/WaitControllerTest.java
+++ timer-service/src/test/java/com/manywho/services/timer/service/controllers/WaitControllerTest.java
@@ -8,19 +8,18 @@ import com.manywho.sdk.entities.run.elements.config.ServiceResponse;
 import com.manywho.sdk.enums.ContentType;
 import com.manywho.sdk.enums.InvokeType;
 import com.manywho.sdk.services.providers.ObjectMapperProvider;
+import com.manywho.sdk.test.FunctionalTest;
+import com.manywho.sdk.utils.AuthorizationUtils;
 import org.glassfish.jersey.client.ClientConfig;
-import org.glassfish.jersey.test.JerseyTest;
 import org.glassfish.jersey.test.TestProperties;
 import org.junit.Assert;
 import org.junit.Test;
 
 import javax.ws.rs.client.Entity;
-import javax.ws.rs.core.Application;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
+import javax.ws.rs.core.*;
 import java.util.UUID;
 
-public class WaitControllerTest extends JerseyTest {
+public class WaitControllerTest extends FunctionalTest {
 
     @Override
     protected Application configure() {
@@ -37,6 +36,9 @@ public class WaitControllerTest extends JerseyTest {
 
     @Test
     public void testAbsolute() throws Exception {
+        MultivaluedMap<String,Object> headers = new MultivaluedHashMap<>();
+        headers.add("Authorization", AuthorizationUtils.serialize(getDefaultAuthenticatedWho()));
+
         String randomToken = UUID.randomUUID().toString();
 
         ServiceRequest serviceRequest = new ServiceRequest() {{
@@ -49,6 +51,7 @@ public class WaitControllerTest extends JerseyTest {
 
         ServiceResponse response = target("wait/absolute")
                 .request(MediaType.APPLICATION_JSON_TYPE)
+                .headers(headers)
                 .post(Entity.json(serviceRequest), ServiceResponse.class);
 
         Assert.assertEquals(InvokeType.Wait, response.getInvokeType());
@@ -57,6 +60,9 @@ public class WaitControllerTest extends JerseyTest {
 
     @Test
     public void testAbsoluteWithInvalidSchedule() throws Exception {
+        MultivaluedMap<String,Object> headers = new MultivaluedHashMap<>();
+        headers.add("Authorization", AuthorizationUtils.serialize(getDefaultAuthenticatedWho()));
+
         String randomToken = UUID.randomUUID().toString();
         final String ENDPOINT = "/wait/absolute";
 
@@ -70,6 +76,7 @@ public class WaitControllerTest extends JerseyTest {
 
         Response response = target(ENDPOINT)
                 .request(MediaType.APPLICATION_JSON_TYPE)
+                .headers(headers)
                 .buildPost(Entity.json(serviceRequest))
                 .invoke();
 
@@ -81,6 +88,9 @@ public class WaitControllerTest extends JerseyTest {
 
     @Test
     public void testRelative() throws Exception {
+        MultivaluedMap<String,Object> headers = new MultivaluedHashMap<>();
+        headers.add("Authorization", AuthorizationUtils.serialize(getDefaultAuthenticatedWho()));
+
         String randomToken = UUID.randomUUID().toString();
 
         ServiceRequest serviceRequest = new ServiceRequest() {{
@@ -93,6 +103,7 @@ public class WaitControllerTest extends JerseyTest {
 
         ServiceResponse response = target("wait/relative")
                 .request(MediaType.APPLICATION_JSON_TYPE)
+                .headers(headers)
                 .post(Entity.json(serviceRequest), ServiceResponse.class);
 
         Assert.assertEquals(InvokeType.Wait, response.getInvokeType());
@@ -101,6 +112,9 @@ public class WaitControllerTest extends JerseyTest {
 
     @Test
     public void testRelativeWithInvalidSchedule() throws Exception {
+        MultivaluedMap<String,Object> headers = new MultivaluedHashMap<>();
+        headers.add("Authorization", AuthorizationUtils.serialize(getDefaultAuthenticatedWho()));
+
         String randomToken = UUID.randomUUID().toString();
         final String ENDPOINT = "/wait/relative";
         ServiceRequest serviceRequest = new ServiceRequest() {{
@@ -113,6 +127,7 @@ public class WaitControllerTest extends JerseyTest {
 
         Response response = target(ENDPOINT)
                 .request(MediaType.APPLICATION_JSON_TYPE)
+                .headers(headers)
                 .buildPost(Entity.json(serviceRequest))
                 .invoke();
 
